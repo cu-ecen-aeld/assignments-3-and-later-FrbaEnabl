5060235b531a22e9987e90f39b5faeaf2460e45a
Frederik Barløse <frba@enabl.dk>
2025-05-05 10:43:47 +0200

    fix 1

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -59,7 +59,7 @@ then
 fi
 
 # TODO: Create necessary base directories
-mkdir -p bin etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p bin etc home lib lib64 proc sbin sys tmp usr var dev
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 
@@ -80,9 +80,7 @@ fi
 # TODO: Make and install busybox
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX=${OUTDIR} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
-echo "DONE SO FAR"
 echo "Library dependencies"
-pwd
 ${CROSS_COMPILE}readelf -a ${OUTDIR}/bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a ${OUTDIR}/bin/busybox | grep "Shared library"
 
