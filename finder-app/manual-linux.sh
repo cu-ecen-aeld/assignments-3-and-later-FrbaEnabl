07c89ace94e77b02a8e1f009aafe0c9006799b58
Frederik Barløse <frba@enabl.dk>
2025-05-05 15:33:17 +0200

    adfg

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -117,5 +117,5 @@ sudo chown -R frba:frba ${OUTDIR}
 # TODO: Create initramfs.cpio.gz
 cd "$OUTDIR/rootfs"
 pwd
-find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+find $OUTDIR/rootfs | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 # gzip -f ${OUTDIR}/initramfs.cpio
