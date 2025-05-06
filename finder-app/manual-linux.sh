8770e933743dfd55094dd503cbda5856fefb386e
Frederik Barløse <frba@enabl.dk>
2025-05-06 14:11:58 +0200

    dfghah

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -7,12 +7,12 @@ set -u
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.1.10
+KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
+export PATH="/usr/local/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin:$PATH"
 CROSS_COMPILE=aarch64-none-linux-gnu-
-
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
