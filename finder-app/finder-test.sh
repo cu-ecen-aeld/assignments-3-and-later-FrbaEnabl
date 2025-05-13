49758ef5b3253a7daf9f024d20f34dcaaf8a715e
Frederik Barløse <frba@enabl.dk>
2025-05-13 11:07:19 +0200

    adfg

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -59,7 +59,7 @@ done
 ls -l
 pwd
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
-
+echo "$OUTPUTSTRING" > "/tmp/assignment4-result.txt"
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
