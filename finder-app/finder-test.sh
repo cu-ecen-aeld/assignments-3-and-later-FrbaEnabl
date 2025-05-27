0e9b2eb5f50cedd7e242b789e361d5e0e0e84fc4
Frederik Barløse <frba@enabl.dk>
2025-05-27 09:01:08 +0200

    fixed finder path

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -58,7 +58,7 @@ do
 done
 ls -l
 pwd
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 echo "$OUTPUTSTRING" > "/tmp/assignment4-result.txt"
 # remove temporary directories
 rm -rf /tmp/aeld-data
