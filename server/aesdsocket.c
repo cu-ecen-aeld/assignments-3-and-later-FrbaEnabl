05206c34e68a2c4029a5f6dcf414a34a5214ec6b
Frederik Barløse <frba@enabl.dk>
2025-05-28 14:07:54 +0200

    adfadh

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -114,7 +114,7 @@ int main() {
             
             // Debugging print to show received data
             printf("Received buffer: %s", buffer);
-            
+            send(fd, buffer, res, 0);
             // while ((newline = strchr(start, '\n')) != NULL) {
                 //     printf("BLAM");
                 //     size_t len = newline - start + 1;
